b4685ad8ec2fa22982a683065d8a14ead3d8461a
ruddroo <mhmruddro1443@gmail.com>
2025-11-01 13:46:52 +0600

    Describe your changes here

---
+++ practice/aiubREgi.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<head>
+
+<title> aiub_registration </title>
+</head>
+<body>
+<center>
+<h1>AIUB COURSE REGISTRATION</h1>
+</center>
+
+     Enter Your Name:
+   <input type="text"> <br><br>
+
+   Enter Your id:
+   <input type="text">  <br><br>
+
+   How much credit do you want:
+   <input type="number">  <br><br>
+
+
+   
+
+   Enter Your section:
+  <input type="checkbox">A 
+   <input type="radio">B
+   <input type="radio">C <br><br>
+
+   Enter Your Perf course: <br><br>
+   <input type="radio">Math1 <br><br>
+   <input type="radio">Web tech <br><br>
+   <input type="radio">Data communication
+
+
+ <input type="submit"> <br><br>
+
+
+
+
+    </body>
