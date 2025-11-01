241a252fbac5945b37536e92ee3f0798589f64a8
ruddroo <mhmruddro1443@gmail.com>
2025-11-01 14:01:08 +0600

    done

---
+++ practicce2.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<head>
+
+<title> aiub_registration </title>
+</head>
+<body>
+<center>
+<h1>AIUB COURSE REGISTRATION</h1>
+ 
+<table>
+<tr>
+    <td>
+     Enter Your Name:
+</td>
+
+<td>
+   <input type="text">  
+</td>
+</tr>
+
+<tr>
+<td>    
+   Enter Your id:
+</td>
+<td>
+   <input type="text">   
+</td>
+</tr>
+
+<tr>
+    <td>
+   How much credit do you want:
+</td>
+<td>
+   <input type="number">   
+</td>
+</tr>
+
+<tr>
+    <td>
+    Select Your prefered section:
+  </td>
+
+  <td>
+    <select>
+      <option></option>
+     <option>A</option>
+     <option>B</option>
+     <option>C</option>
+     <option>D</option>
+     </select>
+</td>
+</tr>
+
+<tr>
+    <td>
+    Enter Your Perf course:  
+</td>
+<td>
+    <input type="radio" name="abb" > Math1  
+   <input type="radio"  name="abb" > Web tech 
+   <input type="radio" name="abb" > Data communication
+   </td>
+   </tr>
+
+
+ 
+
+
+
+
+
+   <tr>
+    <td>
+
+        <center>
+        <input type="submit">
+        </center>  
+</td>
+</tr>
+</table>
+</center>
+</body>
